ddf7539d315edf4f9c7040adc58accf845b54b73
Donato Sapio <student50@eficode-academy-dsk25.(none)>
2012-01-20 16:55:03 +0200

    adding the library project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = '696d993cb15c1466045adbbeb4028239eb5edaf897fb56450a6277a9be925208e8ec5b7ce8deed520eb6c6eaa934a59b93a8cd22d61736c3325d36e746344ae1'
